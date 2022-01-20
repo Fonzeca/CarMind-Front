f41c59bb0cb36feef128d97616736c593821615d
alanrios21 <riosalan264@gmail.com>
2022-01-20 09:11:42 -0300

    CAR 37 Pantalla de inicio de sesion

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
-import { LoginCarrouselComponent } from './login-carrousel/login-carrousel.component';
+
 
 
 
 @NgModule({
   declarations: [
-    LoginFormComponent,
-    LoginCarrouselComponent
+    LoginFormComponent
+
   ],
   imports: [
     CommonModule
