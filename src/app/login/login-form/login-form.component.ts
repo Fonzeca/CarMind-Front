c085aeb5471771ad9d316010786e0e9431815eec
alanrios21 <riosalan264@gmail.com>
2022-01-31 11:17:30 -0300

    CAR 37 Pantalla de inicio de sesion

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -1,45 +1,51 @@
 import { LoginService } from './../../service/login.service';
-import { Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Login } from 'src/app/Interface/login';
 import { Router } from '@angular/router';
-import { catchError, of } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
+  constructor(
+    public formBuilder: FormBuilder,
+    public loginService: LoginService,
 
+  ) {}
 
+  email!: string
 
-  constructor(public formBuilder: FormBuilder, private loginService: LoginService) { }
 
   myForm = this.formBuilder.group({
-    usuario : '',
-    contraseña : ''
-  })
+    usuario: this.loginService.usuario,
+    contraseña: this.loginService.contraseña
+  });
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  verLogin(form: Login): any {
+    this.loginService.login(form).subscribe(catchError(this.handlerError));
   }
 
-verLogin(form: Login){
- 
-   this.loginService.login(form).pipe(
+  handlerError(error: any) {
 
+    console.log(error);
+    return of(error);
+  }
 
-     catchError(this.handlerError)
-   )
+  submit(){
 
-
+  }
 }
 
-handlerError(error: any){
-  /* if(error){
-    this.renderer2.setStyle(this.elementRef.nativeElement, 'border', 'red')
-  } */
-  console.log(error)
-  return of(error)
-}
-
-}
+//Que las validaciones aparescan cuando haga click en el boton de inicio de sesion
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,63 +1,99 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, catchError, map, Observable, of, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  of,
+  throwError,
+} from 'rxjs';
 import { Login } from '../Interface/login';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-
-url : string = 'http://vps-1791261-x.dattaweb.com:2233/';
-logged= new BehaviorSubject<boolean>(false);
-currentUserSubject!: BehaviorSubject<Login>;
-currentUser!: Observable<Login>;
-  constructor(private http:HttpClient , private router: Router) {
-
-    this.currentUserSubject = new BehaviorSubject<Login>(JSON.parse(sessionStorage.getItem('token') || '{}'));
+  usuario = new FormControl('', [Validators.required, Validators.maxLength(50), Validators.pattern('\b[\w.-]+@[\w.-]+.\w{2,4}\b')]);
+  contraseña =  new FormControl('', [Validators.required, Validators.maxLength(50)])
+  resultado!: string;
+  url: string = 'http://vps-1791261-x.dattaweb.com:2233/';
+  logged = new BehaviorSubject<boolean>(false);
+  currentUserSubject!: BehaviorSubject<Login>;
+  currentUser!: Observable<Login>;
+  constructor(private http: HttpClient, private router: Router) {
+    this.currentUserSubject = new BehaviorSubject<Login>(
+      JSON.parse(sessionStorage.getItem('token') || '{}')
+    );
     this.currentUser = this.currentUserSubject.asObservable();
-
   }
 
-login(usuario: Login) : any{
-let path = `login?username=${usuario.usuario}&password=${usuario.contraseña}`;
+  login(usuario: Login): Observable<any> {
+    let path = `login?username=${usuario.usuario}&password=${usuario.contraseña}`;
 
-  return this.http.post<any>(this.url + path, usuario)
-  .pipe(map(data => {
+    return this.http.post<any>(this.url + path, usuario).pipe(
+      map((data) => {
+        sessionStorage.setItem('token', data.token);
 
-    sessionStorage.setItem('token', data.token);
-    
-    this.currentUserSubject.next(data);
+        this.currentUserSubject.next(data);
 
+        this.logged.next(true);
 
-    this.logged.next(true);
+        this.router.navigate(['/home']);
 
-
-
-
-    return data;
+        return data;
+      }),
+      catchError(this.handlerError)
+    );
   }
 
-  ),
-  catchError(this.handlerError)
-  );
+  handlerError(error: any) {
+    console.log(error);
+    return of(error);
+  }
+
+  logout(): void {
+    sessionStorage.removeItem('Usuario');
+    this.logged.next(false);
+  }
+
+  errorUsuario(){
+    if((this.usuario.dirty || this.usuario.touched) && this.usuario.errors){
+      this.resultado = "No ha ingresado su email";
+
+    }
+    if(this.usuario.hasError('pattern') && this.usuario.errors){
+      this.resultado = "El campo esta incorrecto";
+
+    }
 
 
-}
 
-handlerError(error: any){
-  console.log(error)
-  return of(error)
-}
 
-logout(): void{
-  sessionStorage.removeItem('Usuario');
-  this.logged.next(false);
+    else if(this.usuario.hasError('maxlength') && this.usuario.errors){
+      this.resultado = "El maximo de letras es 30";
 
-}
+    }
 
-/* getToken(token: string): any {
+    return this.resultado;
+  }
+
+  errorContrasenia(){
+    if((this.contraseña.dirty || this.contraseña.touched) && this.contraseña.errors){
+      this.resultado = "No ha ingresa su contraseña";
+
+    }
+
+    else if(this.contraseña.hasError('maxlength') && this.contraseña.errors){
+      this.resultado = "El maximo de letras es 30";
+
+    }
+
+    return this.resultado;
+  }
+
+  /* getToken(token: string): any {
   return sessionStorage.getItem('Usuario');
 } */
-
 }
