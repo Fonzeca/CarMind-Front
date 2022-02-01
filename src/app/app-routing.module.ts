1d4dfe5c1d3b3e85f5d44d7a9769058cd3145499
alanrios21 <riosalan264@gmail.com>
2022-02-01 12:33:20 -0300

    CAR 40 Pantalla Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+
 import { HomeComponent } from './vehicles/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
+import { HomeComponent } from './vehicles/home/home.component';
+
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { LoginModule } from './login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SidebarComponent } from './vehicles/sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LoginModule,
     HttpClientModule,
+    
+
 
   ],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
