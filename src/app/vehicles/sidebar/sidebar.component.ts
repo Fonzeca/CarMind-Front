cfc303fad1c1f735c13df605ef8738ab43e1fa0e
alanrios21 <riosalan264@gmail.com>
2022-02-08 11:00:09 -0300

    CAR 40 Pantalla Home

--- src/app/vehicles/sidebar/sidebar.component.ts
+++ src/app/vehicles/sidebar/sidebar.component.ts
@@ -61,16 +61,40 @@ export class SidebarComponent implements OnInit {
   abrirSidebar() {
     const asRigth = this.toggleRigth.nativeElement;
     const asToggle = this.toggle.nativeElement;
+    const asButtonLeft = this.toggleButtonLeft.nativeElement;
+    const asButtonRigth = this.toggleButtonRigth.nativeElement;
+    const asImgMovie = this.toggleImgMovie.nativeElement;
     this.renderer2.setStyle(asToggle, 'width', '21.3%');
     this.renderer2.setStyle(asRigth, 'margin-left', '300px');
+    this.renderer2.setStyle(asButtonLeft, 'right', '290px');
+    this.renderer2.setStyle(asButtonRigth, 'left', '310px');
+    this.renderer2.addClass(asButtonLeft, 'pepito');
+    this.renderer2.addClass(asButtonRigth, 'susano');
+    this.renderer2.addClass(asButtonLeft, 'naruto');
+    this.renderer2.addClass(asButtonRigth, 'sasuke');
+    this.renderer2.addClass(asImgMovie, 'search-movie');
+    this.renderer2.addClass(asRigth, 'clase-cualquiera');
+    this.renderer2.addClass(asRigth, 'otra-clase');
+
 
   }
 
   cerrarSidebar() {
     const asToggle = this.toggle.nativeElement;
     const asRigth = this.toggleRigth.nativeElement;
+    const asButtonLeft = this.toggleButtonLeft.nativeElement;
+    const asButtonRigth = this.toggleButtonRigth.nativeElement;
+    const asImgMovie = this.toggleImgMovie.nativeElement;
     this.renderer2.setStyle(asToggle, 'width', '88px');
-    this.renderer2.setStyle(asRigth, 'margin-left', '0px');
+    this.renderer2.setStyle(asRigth, 'margin-left', '100px');
+    this.renderer2.setStyle(asButtonLeft, 'right', '290px');
+
+      this.renderer2.setStyle(asButtonRigth, 'left', '315px');
+      this.renderer2.removeClass(asButtonLeft, 'pepito');
+      this.renderer2.removeClass(asButtonRigth, 'susano');
+      this.renderer2.removeClass(asButtonLeft, 'naruto');
+      this.renderer2.removeClass(asButtonRigth, 'sasuke');
+      this.renderer2.removeClass(asImgMovie, 'search-movie');
   }
 
   cambiarIndicator(ele: HTMLElement) {}
