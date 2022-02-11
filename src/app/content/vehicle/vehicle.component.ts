6dc8470417344cd51c31b12a356dd43b74a49edf
alanrios21 <riosalan264@gmail.com>
2022-02-11 13:36:07 -0300

    CAR 44 Pantalla Vehiculos

--- src/app/content/vehicle/vehicle.component.ts
+++ src/app/content/vehicle/vehicle.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/modal/modal.component';
 
 
 
@@ -9,13 +11,23 @@ import { Component, OnInit, Renderer2 } from '@angular/core';
 })
 export class VehicleComponent implements OnInit {
 
-  constructor(private renderer2: Renderer2) {}
+  constructor(public dialog: MatDialog, private renderer2: Renderer2) {}
 
   openDialog() {
     const body = document.querySelector(".body")
 
    /*  this.renderer2.setStyle(body, 'background-color', 'rgba(0,0,0,0.4)')
     this.renderer2.setStyle(body, 'z.index', '1') */
+
+    this.dialog.open(ModalComponent, {
+      width: '635px;',
+      height: '477px',
+      panelClass: ['md:w-3/5', 'w-full'],
+      maxHeight: '85vh',
+       data: {
+         animal: 'panda',
+       },
+     });
 
   }
 
