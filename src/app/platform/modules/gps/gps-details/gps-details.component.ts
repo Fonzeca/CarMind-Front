0037327b6a7662f6f3a1ba16b4e1f6619c3c88cc
John Montaño <johnfabiomb@gmail.com>
2022-09-15 10:05:28 +0200

    small changes

--- src/app/platform/modules/gps/gps-details/gps-details.component.ts
+++ src/app/platform/modules/gps/gps-details/gps-details.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { vehicle } from 'src/app/platform/interfaces/vehicle';
+import { BaseComponent } from 'src/app/platform/shared/components/base.component';
 
 @Component({
   selector: 'app-gps-details',
   templateUrl: './gps-details.component.html',
   styleUrls: ['./gps-details.component.scss']
 })
-export class GpsDetailsComponent implements OnInit {
+export class GpsDetailsComponent extends BaseComponent implements OnInit {
 
   @Input() vehicle!:vehicle;
 
-  constructor() { }
+  constructor() {
+    super();
+   }
 
   ngOnInit(): void {
   }
